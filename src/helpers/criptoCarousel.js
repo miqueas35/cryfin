fd7a261a3425b07687bb010716a78a9efa7c0c62
Juan Agustin Alejandro <juan_agustin_alejandro@hotmail.com>
2022-04-17 02:28:44 -0300

    style carrousel Cripto

--- src/helpers/criptoCarousel.js
+++ src/helpers/criptoCarousel.js
@@ -1,6 +1,7 @@
-export const getCoinCarousel = async () => {
+export const getCoinCarousel = async (index) => {
+  const cripto = ['DAI,BTC,ETH,USDT,BNB','LUNA,AVAX,SHIB,MATIC,CRO']
   const response = await fetch(
-    "https://api.covalenthq.com/v1/pricing/tickers/?quote-currency=USD&format=JSON&tickers=DAI,BTC,ETH,USDT,BNB,LUNA,AVAX,SHIB,MATIC,CRO&key=ckey_2870fbe91ac146269ee93e49226"
+    `https://api.covalenthq.com/v1/pricing/tickers/?quote-currency=USD&format=JSON&tickers=${cripto[index]}&key=ckey_2870fbe91ac146269ee93e49226`
   );
   const result = await response.json();
   const { data } = result;
