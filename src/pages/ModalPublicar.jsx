f1c994f1f7298c70dd35b0c7c4c5983b62062dd9
Miqueas Acevedo <miqueasacevedo2017@gmail.com>
2022-04-24 15:07:49 -0300

    modificando modal

--- src/pages/ModalPublicar.jsx
+++ src/pages/ModalPublicar.jsx
@@ -8,7 +8,14 @@ const ModalPublicar = () => {
     <div>
         <h2>Modal</h2>
         <button onClick={openModal}>Nuevo Proyecto</button>
-        <Modal isOpen={isOpenModal} closeModal={closeModal}/>
+        <Modal isOpen={isOpenModal} closeModal={closeModal}>
+          <h3>Publica tú proyecto</h3>
+          <img src="https://placeimg.com/350/350/tech" alt="Tech" />
+          <p>aqui va la descripcion del proyecto</p>
+          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button className="btn btn-success me-md-2" type="button">Publicar</button>
+</div>
+          </Modal>
     </div>
   )
 }
