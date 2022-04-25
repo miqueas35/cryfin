3da5a6053308c7f3d429463cd1bbde161e0635de
Miqueas Acevedo <miqueasacevedo2017@gmail.com>
2022-04-25 03:09:32 -0300

    terminando logica de form registro

--- src/pages/ModalPublicar.jsx
+++ src/pages/ModalPublicar.jsx
@@ -7,11 +7,11 @@ const ModalPublicar = () => {
   return (
     <div>
         <h2>Modal</h2>
-        <button onClick={openModal}>Nuevo Proyecto</button>
+        <button type="button" className="btn btn-success" onClick={openModal}>Nuevo Proyecto</button>
         <Modal isOpen={isOpenModal} closeModal={closeModal}>
           <h3>Publica tú proyecto</h3>
           <img src="https://placeimg.com/350/350/tech" alt="Tech" />
-          <p>aqui va la descripcion del proyecto</p>
+          <textarea className="my-2" name="" id="" cols="45" rows="5" placeholder="Aquí va la descripcion de tú proyecto"></textarea>
           <div className="d-grid gap-2 d-md-flex justify-content-md-end">
   <button className="btn btn-success me-md-2" type="button">Publicar</button>
 </div>
