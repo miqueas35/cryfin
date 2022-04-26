1fadeb3ec9ed3df6e6b10962f360ac46efc3ecd3
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2022-04-26 07:55:03 -0300

    primer merge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
-
+const contenedor = document.querySelector("#root");
 ReactDOM.render(<App />, contenedor);
