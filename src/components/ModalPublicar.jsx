d585fdeda96cbae4d11508617e85a65d54a20056
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2022-04-27 22:51:10 -0300

    commit final version 1.0.0

--- src/components/ModalPublicar.jsx
+++ src/components/ModalPublicar.jsx
@@ -11,15 +11,23 @@ const ModalPublicar = () => {
       <div>
         <NavbarCryFIn />
       </div>
-      <h2 className="text-center m-2">Encuentra Inversores Aquí</h2>
-      <div className="text-center">
-        <button type="button" className="btn btn-success" onClick={openModal}>
-          Publica tú Nuevo Proyecto!!!
-        </button>
+      <div className="container cont-home">
+        <div className="row">
+          <div className="col-6 offset-3">
+            <h2 className="display-5 text-center my-3">
+              Encuentra inversores aquí
+            </h2>
+            <div className="text-center">
+              <button type="button" className="btn" onClick={openModal}>
+                Publica tu Nuevo Proyecto
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
       <Modal isOpen={isOpenModal} closeModal={closeModal}>
-        <h3>Publica tú proyecto</h3>
+        <h3>Publica tu proyecto</h3>
         <img src="https://placeimg.com/350/350/tech" alt="Tech" />
         <textarea
           className="my-2"
@@ -27,14 +35,41 @@ const ModalPublicar = () => {
           id=""
           cols="45"
           rows="5"
-          placeholder="Aquí va la descripcion de tú proyecto"
+          placeholder="Aquí va la descripcion de tu proyecto"
         ></textarea>
-        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-          {
-            <Link className="btn btn-success me-md-2" to="/projects">
-              Publicar
-            </Link>
-          }
+
+        <div className="row">
+          <div className="d-grid gap-2 col-12 mx-auto my-5 form-group w-100 text-center">
+            <div class="input-wrapper">
+              <input
+                className="input w-100"
+                type="text"
+                name="inversion"
+                placeholder="Monto solicitado"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="d-grid gap-2 col-12 mx-auto my-5 form-group w-100 text-center">
+            <div class="input-wrapper">
+              <input
+                className="input w-100"
+                type="text"
+                name="billetera"
+                placeholder="Ingrese su billetera"
+                required
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="d-grid gap-2 col-6 mx-auto my-5">
+              <Link className="btn" to="/projects">
+                Publicar
+              </Link>
+            </div>
+          </div>
         </div>
       </Modal>
       <div>
